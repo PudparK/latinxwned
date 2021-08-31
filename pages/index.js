c8da3c3bc2955c4a1a13ed6affd81478221630a1
pudpark <p.barron@outlook.com>
2021-08-30 20:55:07 -0500

    Refactor button.

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 text-center">
+      <main className="flex flex-col items-center justify-center w-full flex-1 text-center p-4">
         <h1 className="text-4xl md:text-6xl font-bold ">
           <span className="block text-3xl">Welcome to</span>
           <span className="text-blue-400">latinxwned.com</span>
@@ -17,8 +17,7 @@ export default function Home() {
         <p className="mt-3 text-2xl text-gray-500">
           We're building a network to help folx find <span className="md:block">Latinx owned businesses.</span>
         </p>
-        <a href="https://mailchi.mp/69ade872cfd3/latinxwned-signup
-        " className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-6">Stay Tuned</a>
+        <a href="https://mailchi.mp/69ade872cfd3/latinxwned-signup" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-6">Stay Tuned</a>
       </main>
 
       {/*<footer className="flex items-center justify-center w-full h-24 border-t">
